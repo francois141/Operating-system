d1b216e1a4ddc533fcec8e7b119e0c89cf90544d
francois141 <frankost.costa@gmail.com>
2022-11-05 19:58:40 +0100

    feature: implementation of pow & sqrt function

--- system_lib/math.c
+++ system_lib/math.c
@@ -15,6 +15,23 @@ float cos(float angle)
 float sqrt(float x)
 {
     float ret = 0.0;
-    __asm__("fcos" : "=t"(ret) : "0"(x));
+    __asm__("fsqrt" : "=t"(ret) : "0"(x));
     return ret;
+}
+
+int pow(int base, int exp)
+{
+    int result = 1;
+    
+    while(1)
+    {
+        if (exp & 1)
+            result *= base;
+        exp >>= 1;
+        if (!exp)
+            break;
+        base *= base;
+    }
+
+    return result;
 }
