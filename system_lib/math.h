cbddcc476cc44754929c91762945a683c34725e7
francois141 <frankost.costa@gmail.com>
2022-11-05 20:00:42 +0100

    feature: update header of math.h

--- system_lib/math.h
+++ system_lib/math.h
@@ -9,8 +9,9 @@
 #define PI 3.14
 #define pi 3.14
 
-float sin(float);
-float cos(float);
-float sqrt(float); 
+float sin(float angle);
+float cos(float angle);
+float sqrt(float x); 
+int pow(int base, int exp);
 
 #endif
